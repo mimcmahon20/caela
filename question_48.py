c038dd657a33a5ad281290d6afddb99cfdd08f36
Maguire McMahon <mimcmahon20@vt.edu>
2025-11-15 12:36:58 -0800

    qs

---
+++ question_48.py
@@ -0,0 +1,16 @@
+# Question 48 (0.5 pts)
+#
+# For load case 5, calculate the shear force V_yLC5(x) at x=7.2ft.
+#
+# Provide your answer in lbs up to 2 places of decimals.
+#
+# Hint:
+#
+# Load case 5 (+3g pull-up) includes both the lift load and the effects of inertia.
+# The calculations will involve the lift distribution along with the inertia (dead load:
+# fuel and bomb, ignoring self weight for now) due to the load factor.
+#
+# To account for the effect of 3g during this load case, you need to scale your loads
+# by factor of 3.
+
+
---
+++ question_49.py
@@ -0,0 +1,7 @@
+# Question 49 (0.5 pts)
+#
+# For load case 5, calculate the bending moment M_zLC5(x) at x=7.6ft.
+#
+# Provide your answer in 10^3 lb-inch up to 2 places of decimals.
+
+
